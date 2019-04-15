49f0b9f2a4db37b03d9339c270fb5adf3a35d02b
mrxy001 <wxy19980615@gmail.com>
2019-04-15 17:23:20 +0800

    搭建数据库

--- index.php
+++ index.php
@@ -1,42 +1,9 @@
-﻿<?php
-<<<<<<< HEAD
-require "public_module.php";
-echo $name
-
-/*$mysql_server_name='localhost'; //改成自己的mysql数据库服务器
-$mysql_username="root"; //改成自己的mysql数据库用户名
-$mysql_password="root"; //改成自己的mysql数据库密码
-$mysql_database="test"; //改成自己的mysql数据库名
-$conn=new mysqli($mysql_server_name,$mysql_username,$mysql_password); //连接数据库
-if($conn==false) {echo "数据连接失败！<br/>"; }
-else {echo "数据连接成功！<br/>"; }
-
-$conn->select_db($mysql_database); //打开数据库*/
-// $conn->query("set names 'utf8'"); //数据库输出编码 应该与你的数据库编码保持一致
-/*$sql ="select * from biao"; //SQL语句
-$result = $conn->query($sql); //查询成功
-$data=array();
-
-while ($tmp=$result->fetch_assoc())
-{
-    // $data[]=$tmp;
-    echo $tmp['name'] . "<br/>";
-}
-*/
+﻿<HTML>
+<head>
+	<title>显示页面</title>
+	<link rel="stylesheet" href="//cdnjs.loli.net/ajax/libs/mdui/0.4.1/css/mdui.min.css">
+	<script src="//cdnjs.loli.net/ajax/libs/mdui/0.4.1/js/mdui.min.js"></script>
+</head>
 
 
-$sql = 'select * from biao';
-var_dump(select($sql));
-
-=======
-
-require "public_module.php";
-
-$sql = "INSERT INTO biao (`name`) values ('newname')";
-// $sql = "SELECT * from biao";
-
-// $con = mysqli_connect("localhost", "root", "root");
-// mysqli_select_db("test", $con);
-
-query($sql);
->>>>>>> 7db7a4967f8adffa01ee200c6dca20b3af8795c8
+</HTML>
---
+++ install.sql
@@ -0,0 +1,55 @@
+-- 创建用户表
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+	`user_id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+	`wx_id` varchar(255) COMMENT '微信ID',
+	`username` varchar(255) NOT NULL COMMENT '用户账号',
+	`password` varchar(255) NOT NULL COMMENT '用户密码',
+	`nickname` varchar(255) COMMENT '用户昵称',
+	`mobile` varchar(255) COMMENT '手机号',
+	`email` varchar(255) COMMENT '邮箱',
+	`create_time` bigint COMMENT '创建时间',
+	`update_time` bigint COMMENT '修改时间',
+	PRIMARY KEY(`user_id`)
+)ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- 创建素材表
+DROP TABLE IF EXISTS `parttime`;
+CREATE TABLE `parttime` (
+	`parttime_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+	`user_id` int(11) COMMENT '上传者ID',
+	`type` varchar(255) NOT NULL COMMENT '类型',
+	`title` varchar(255) NOT NULL COMMENT '标题',
+	`brief` varchar(255) NOT NULL COMMENT '简介',
+	`wage` int(11) NOT NULL COMMENT '时薪',
+	`require` varchar(255) NOT NULL COMMENT '要求',
+	`contact` varchar(255) NOT NULL COMMENT '联系方式(手机)',
+	`linkman` varchar(255) NOT NULL COMMENT '联系人',
+	`read_count` int(11) DEFAULT 0 COMMENT '浏览量',
+	`create_time` bigint COMMENT '发布时间',
+	`update_time` bigint COMMENT '修改时间',
+	PRIMARY KEY(`parttime_id`)
+)ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- 创建浏览记录
+DROP TABLE IF EXISTS `browser`;
+CREATE TABLE `browser` (
+	`browser_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+	`parttime_id` int(11) COMMENT '兼职信息ID',
+	`user_id` int(11) COMMENT '上传者ID',
+	`create_time` bigint COMMENT '浏览时间',
+	`update_time` bigint COMMENT '修改时间',
+	PRIMARY KEY(`browser_id`)
+)ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- 创建评论记录
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment` (
+	`comment_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+	`parttime_id` int(11) COMMENT '兼职信息ID',
+	`user_id` int(11) COMMENT '用户ID',
+	`content` varchar(255) NOT NULL COMMENT '评论内容',
+	`create_time` bigint COMMENT '评论时间',
+	`update_time` bigint COMMENT '修改时间',
+	PRIMARY KEY(`comment_id`)
+)ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;

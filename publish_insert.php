f7b63fcfcd65151107facf8f93b4edb4a745a27d
mrxy001 <wxy19980615@gmail.com>
2019-04-19 20:00:07 +0800

    添加H5微信朋友圈模板；判断标题和简介是否为空

--- publish_insert.php
+++ publish_insert.php
@@ -9,15 +9,19 @@ if (!isset($_SESSION['user_id']))
 }
 $user_id = $_SESSION['user_id'];
 $type = seize('type');
+$title = seize('title');
 $brief = seize('brief');
 $wage = seize('wage');
 $require = seize('require');
 $contact = seize('contact');
 $linkman = seize('linkman');
 
-$sql = "INSERT INTO parttimes (user_id,type,brief,wage,`require`,contact,linkman) values ('$user_id', '$type', '$brief', '$wage', '$require', '$contact', '$linkman')";
+if ($title == '' || $brief == '')
+	exit;
+
+$sql = "INSERT INTO parttimes (user_id,type,title,brief,wage,`require`,contact,linkman) values ('$user_id', '$type', '$title', '$brief', '$wage', '$require', '$contact', '$linkman')";
 $result = query2($sql, 0);
 
-echo "添加成功";
+echo "兼职信息添加成功";
 
 
